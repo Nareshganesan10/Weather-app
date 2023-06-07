197a9b141417182d891b992c30b70df578fcd2ea
Vairava Naresh <vairavanareshganesan@gmail.com>
2023-06-07 12:29:17 +0530

    Added more cities

--- lookup/views.py
+++ lookup/views.py
@@ -8,10 +8,7 @@ from django.views.decorators import csrf
 def home(request):
     if request.method == "POST":
         city = request.POST['City']
-        print(city)
         api_request = requests.get("http://api.weatherapi.com/v1/current.json?key=9ac471934a0b45468c761301230706&q="+ str(city) + "&aqi=yes")
-        print("http://api.weatherapi.com/v1/current.json?key=9ac471934a0b45468c761301230706&q="+ str(city) + "&aqi=yes")
-        # api_request = requests.get("http://api.weatherstack.com/current?access_key=be85b86fb0c2beb5b0b9244d2903db8d&query=" + str(city))
     try:
         api = json.loads(api_request.content)
     except Exception as e:
